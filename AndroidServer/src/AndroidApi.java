5b15b8f83507d84a7c2e6bb9900df795f6b44c3f
jaky <weijunjie@onyx-international.com>
2018-02-09 20:31:41 +0800

    init import.

---
+++ AndroidServer/src/AndroidApi.java
@@ -0,0 +1,29 @@
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class AndroidApi extends HttpServlet {
+    private String message;
+
+    @Override
+    public void init() throws ServletException {
+        message = "Hello world, this message is from servlet!";
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setCharacterEncoding("UTF-8");
+        response.setContentType("application/json; charset=utf-8");//返回数据格式为json
+
+        PrintWriter out = response.getWriter();
+        out.println("<h3>" + message + "</h3>");
+    }
+
+    @Override
+    public void destroy() {
+        super.destroy();
+    }
+}
